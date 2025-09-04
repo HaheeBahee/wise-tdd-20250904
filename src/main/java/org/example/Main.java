c5d0a37fafc2962d2ffc722c2ce3e7e3168fba54
HaheeBahee <parkadg@naver.com>
2025-09-04 14:22:35 +0900

    ByteArrayOutputStream에 출력 저장

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 
@@ -8,10 +10,32 @@ public class Main {
     public static void main(String[] args) {
 
         experiment1();
+        experiment2();
+
 
     }
 
-    public static void experiment1(){
+    public static void experiment2() {
+        System.out.println("안녕하세요");
+
+        PrintStream originalOut = System.out;
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+
+        System.setOut(printStream);
+
+        System.out.println("하하하");
+
+        String outStr = outputStream.toString();
+        System.setOut(originalOut);
+        printStream.close();
+
+        System.out.println(outStr);
+
+    }
+
+    public static void experiment1() {
         String input = """
                 등록
                 너 자신을 알라
